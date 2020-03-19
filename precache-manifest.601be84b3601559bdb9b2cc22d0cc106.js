509c45e94a2d623a9e822dbdc9055546e9db1bab
Himanshu Kumar <himanshu.kumar@betterplace.co.in>
2020-03-19 15:09:42 +0530

    Updates

---
+++ precache-manifest.601be84b3601559bdb9b2cc22d0cc106.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda2b974c0e8c3f55c56646863fb865d",
+    "url": "/infratab/index.html"
+  },
+  {
+    "revision": "fe49f6ad1f870227cd01",
+    "url": "/infratab/static/css/main.4e1693c8.chunk.css"
+  },
+  {
+    "revision": "39df43d0db8e6fe82a34",
+    "url": "/infratab/static/js/2.b7dfd437.chunk.js"
+  },
+  {
+    "revision": "96219bc210d065416bcec57f9037d9d9",
+    "url": "/infratab/static/js/2.b7dfd437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe49f6ad1f870227cd01",
+    "url": "/infratab/static/js/main.75971f64.chunk.js"
+  },
+  {
+    "revision": "f1da59b24d6f4ac663e7",
+    "url": "/infratab/static/js/runtime-main.82029c62.js"
+  },
+  {
+    "revision": "f697887c650f2985e505965cd6e4e68d",
+    "url": "/infratab/static/media/left.f697887c.svg"
+  },
+  {
+    "revision": "8dad80289a1ece380591ad143b616747",
+    "url": "/infratab/static/media/logo.8dad8028.svg"
+  },
+  {
+    "revision": "01769d756b48ae2f998468a878d69783",
+    "url": "/infratab/static/media/right.01769d75.svg"
+  }
+]);
